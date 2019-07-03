f11f6bcc5d49f8c50323d686c0c8e43e19632bea
Richard Mann <31983230+safepay@users.noreply.github.com>
2019-07-03 13:19:09 +1000

    Initial Commit

---
+++ custom_components/willyweather/__init__.py
@@ -0,0 +1,1 @@
+"""The willyweather component."""
---
+++ custom_components/willyweather/sensor.py
@@ -0,0 +1,198 @@
+"""Support for the WillyWeather Australia service."""
+import logging
+from datetime import timedelta
+
+import requests
+import voluptuous as vol
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.const import (
+    TEMP_CELSIUS, CONF_MONITORED_CONDITIONS, CONF_NAME,
+    ATTR_ATTRIBUTION)
+from homeassistant.helpers.entity import Entity
+from homeassistant.util import Throttle
+
+_RESOURCE = 'https://api.willyweather.com.au/v2/{}/locations/{}/weather.json?observational=true'
+_LOGGER = logging.getLogger(__name__)
+
+ATTRIBUTION = "Weather details provided by WillyWeather Australia."
+
+#CONF_FORECAST = 'forecast'
+CONF_STATION_ID = 'station_id'
+CONF_API_KEY = 'api_key'
+
+DEFAULT_NAME = 'WW'
+
+MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=10)
+
+SENSOR_TYPES = {
+    'temperature': ['Temperature', 'C'],
+    'apparent_temperature': ['Feels like', 'C'],
+    'cloud': ['Cloud', 'Oktas'],
+    'humidity': ['Humidity', '%'],
+    'dewpoint': ['Dew point', 'C'],
+    'pressure': ['Pressure', 'hPa'],
+    'wind_speed': ['Wind speed', 'km/h'],
+    'wind_gust': ['Wind gust', 'km/h'],
+    'wind_direction': ['Wind direction', None],
+    'rainlasthour': ['Rain last hour', 'mm'],
+    'raintoday': ['Rain today', 'mm'],
+    'rainsince9am': ['Rain since 9am', 'mm']
+}
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_API_KEY): cv.string,
+    vol.Required(CONF_STATION_ID): cv.string,
+    vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
+    vol.Required(CONF_MONITORED_CONDITIONS, default=[]):
+        [vol.In(SENSOR_TYPES)],
+})
+
+
+def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the Yahoo! weather sensor."""
+    #from yahooweather import get_woeid, UNIT_C, UNIT_F
+
+    #unit = hass.config.units.temperature_unit
+    station_id = config.get(CONF_STATION_ID)
+    api_key = config.get(CONF_API_KEY)
+    #forecast = config.get(CONF_FORECAST)
+    name = config.get(CONF_NAME)
+
+    #yunit = UNIT_C if unit == TEMP_CELSIUS else UNIT_F
+
+    #SENSOR_TYPES['temperature'][1] = unit
+    #SENSOR_TYPES['apparent_temperature'][1] = unit
+
+    # If no station_id determine from Home Assistant lat/long
+    if station_id is None:
+        station_id = get_station_id(hass.config.latitude, hass.config.longitude)
+        if station_id is None:
+            _LOGGER.critical("Can't retrieve Station from WillyWeather")
+            return False
+
+    ww_data = WeatherData(api_key, station_id)
+
+    try:
+        ww_data.update()
+    except ValueError as err:
+        _LOGGER.error("Received error from WillyWeather: %s", err)
+        return
+
+#    if not ww_data.update():
+#        _LOGGER.critical("Can't retrieve weather data from WillyWeather")
+#        return False
+
+#    if forecast >= len(yahoo_api.yahoo.Forecast):
+#        _LOGGER.error("Yahoo! only support %d days forecast!",
+#                      len(yahoo_api.yahoo.Forecast))
+#        return False
+
+    dev = []
+    for variable in config[CONF_MONITORED_CONDITIONS]:
+        dev.append(WWWeatherSensor(ww_data, name, variable))
+
+    add_entities(dev, True)
+
+
+class WWWeatherSensor(Entity):
+    """Implementation of the WillyWeather weather sensor."""
+
+    def __init__(self, weather_data, name, sensor_type):
+        """Initialize the sensor."""
+        self._client = name
+        self._name = SENSOR_TYPES[sensor_type][0]
+        self._type = sensor_type
+        self._state = None
+        self._unit = SENSOR_TYPES[sensor_type][1]
+        self._data = weather_data
+        self._code = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return '{} {}'.format(self._client, self._name)
+
+    @property
+    def state(self):
+        """Return the state of the device."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self):
+        """Return the unit of measurement of this entity, if any."""
+        return self._unit
+
+    @property
+    def device_state_attributes(self):
+        """Return the state attributes."""
+        attrs = {ATTR_ATTRIBUTION: ATTRIBUTION}
+        return attrs
+
+    def update(self):
+        """Get the latest data from WillyWeather and updates the states."""
+        self._data.update()
+        if not self._data:
+            _LOGGER.info("Didn't receive weather data from WillyWeather")
+            return
+
+        # Read data
+        if self._type == 'temperature':
+            self._state = self._data.latest_data["observations"]["temperature"].get("temperature")
+        elif self._type == 'apparent_temperature':
+            self._state = self._data.latest_data["observations"]["temperature"].get("apparentTemperature")
+        elif self._type == 'cloud':
+            self._state = self._data.latest_data["observations"]["cloud"].get("oktas")
+        elif self._type == 'humidity':
+            self._state = self._data.latest_data["observations"]["humidity"].get("percentage")
+        elif self._type == 'depoint':
+            self._state = self._data.latest_data["observations"]["dewPoint"].get("temperature")
+        elif self._type == 'pressure':
+            self._state = self._data.latest_data["observations"]["pressure"].get("pressure")
+        elif self._type == 'wind_speed':
+            self._state = self._data.latest_data["observations"]["wind"].get("speed")
+        elif self._type == 'wind_gust':
+            self._state = self._data.latest_data["observations"]["wind"].get("gustSpeed")
+        elif self._type == 'wind_direction':
+            self._state = self._data.latest_data["observations"]["wind"].get("directionText")
+        elif self._type == 'rainlasthour':
+            self._state = self._data.latest_data["observations"]["rainfall"].get("lastHourAmount")
+        elif self._type == 'raintoday':
+            self._state = self._data.latest_data["observations"]["rainfall"].get("todayAmount")
+        elif self._type == 'rainsince9am':
+            self._state = self._data.latest_data["observations"]["rainfall"].get("since9AMAmount")
+
+
+class WeatherData:
+    """Handle WillyWeather API object and limit updates."""
+
+    def __init__(self, api_key, station_id):
+        """Initialize the data object."""
+        self._api_key = api_key
+        self._station_id = station_id
+
+    def _build_url(self):
+        """Build the URL for the requests."""
+        url = _RESOURCE.format(self._api_key, self._station_id)
+        _LOGGER.debug("WillyWeather URL: %s", url)
+        return url
+
+    @property
+    def latest_data(self):
+        """Return the latest data object."""
+        if self._data:
+            return self._data
+        return None
+
+    #@property
+    #def willyweather(self):
+    #    """Return WillyWeather API object."""
+    #    return self._willyweather
+
+    @Throttle(MIN_TIME_BETWEEN_UPDATES)
+    def update(self):
+        """Get the latest data from WillyWeather."""
+        result = requests.get(self._build_url(), timeout=10).json()
+        self._data = result['observational']
+        return
