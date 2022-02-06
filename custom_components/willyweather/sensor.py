208d980b2a9e9a0fafa7cc65cac09540996a3ede
Riccardo <31983230+safepay@users.noreply.github.com>
2022-02-06 19:28:02 +1100

    Changed device_state_attributes to extra_state_attributes

--- custom_components/willyweather/sensor.py
+++ custom_components/willyweather/sensor.py
@@ -224,7 +224,7 @@ class WWWeatherSensor(Entity):
         return self._unit
 
     @property
-    def device_state_attributes(self):
+    def extra_state_attributes(self):
         """Return the state attributes."""
         attrs = {ATTR_ATTRIBUTION: ATTRIBUTION}
         return attrs
