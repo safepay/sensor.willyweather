eb8da9253fa754fefa34a9af422c6ab28fd41424
Richard <31983230+safepay@users.noreply.github.com>
2025-10-13 09:12:34 +1100

    Update __init__.py

--- custom_components/willyweather/__init__.py
+++ custom_components/willyweather/__init__.py
@@ -1,1 +1,35 @@
-"""The willyweather component."""
+"""The WillyWeather integration."""
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+_LOGGER = logging.getLogger(__name__)
+
+DOMAIN = "willyweather"
+PLATFORMS = ["sensor", "weather"]
+
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
+    """Set up the WillyWeather component."""
+    hass.data.setdefault(DOMAIN, {})
+    return True
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up WillyWeather from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
